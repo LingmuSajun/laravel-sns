e3360341509ef7c3fe942b46b58f65cfe3c00f55
鈴木颯駿 <soshunsuzuki@soshun-mb.local>
2020-05-16 13:07:49 +0900

    記事投稿機能作成

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Auth::routes();
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
