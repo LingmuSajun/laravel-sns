94994ecf8af1e25be198eab6bfc67798dca7667c
鈴木颯駿 <soshunsuzuki@soshun-mb.local>
2020-05-18 02:03:27 +0900

    記事詳細画面の表示

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -43,4 +43,9 @@ class ArticleController extends Controller
         $article->delete();
         return redirect()->route('articles.index');
     }
+
+    public function show(Article $article)
+    {
+        return view('articles.show', ['article' => $article]);
+    }
 }
